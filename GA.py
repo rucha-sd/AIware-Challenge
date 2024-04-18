f4acd6d02f75f4caab0286bc619a125d88e20d1c
Rucha <rucha.d2000@gmail.com>
2024-04-17 20:22:31 -0400

    Solved Errors

--- GA.py
+++ GA.py
@@ -1,11 +1,15 @@
 import random
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import copy
 
 class Paper:
     def __init__(self, id, authors, duration, topic=None):
         self.id = id
         self.authors = authors
         self.duration = duration
-        self.topic = topic
+        topics = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.topic = random.choice(topics) if topic is None else topic
 
 class Session:
     def __init__(self, max_length, tracks):
@@ -21,21 +25,20 @@ class Solution:
         time_penalty = 0
         author_penalty = 0
         distribution_penalty = 0
-
-        # total_possible_duration = sum(session.max_length for session in self.sessions)
         total_time_unused = 0
         papers_scheduled = set() 
+        total_authors = set()
 
         for session in self.sessions:
             actual_duration = 0
-            total_time_unused = 0
+            session_time_unused = 0
             # create a list of sets that contains authors in each track
             authors_in_session = [set() for _ in range(len(session.tracks))]
             for i in range(len(session.tracks)):
                 track = session.tracks[i]
                 track_duration = sum(paper.duration for paper in track)
 
-                total_time_unused += session.max_length - track_duration
+                session_time_unused += max(0, session.max_length - track_duration)
                 # for paper in track:    
                     # Penalize if a track exceeds the session's max_length
                 if track_duration > session.max_length:
@@ -47,20 +50,48 @@ class Solution:
                     for author in paper.authors:
                         # check if the author is present in any of the previous tracks
                         authors_in_session[i].add(author)
+                    total_authors.update(paper.authors)
                                 
             # make a list that contains all the authors in the session, keep duplicates
-            all_authors = [author in authors_in_session[i] for i in range(len(session.tracks))]
+            # print(len(session.tracks))
+            all_authors = []
+            for authors_set in authors_in_session:
+                all_authors.extend(authors_set)
             # Penalize for repeating authors
             author_penalty += len(all_authors) - len(set(all_authors))
+            total_time_unused += session_time_unused
 
         # Penalize if a paper is not scheduled at all
-        distribution_penalty += num_papers - len(papers_scheduled)
+        distribution_penalty = num_papers - len(papers_scheduled)
         # print all penalties for the solution
-        print(f"Time Penalty: {time_penalty}, Author Penalty: {author_penalty}, Distribution Penalty: {distribution_penalty}, Duration Penalty: {total_time_unused}")
+        # print(f"Time Penalty: {time_penalty}, Author Penalty: {author_penalty}, Distribution Penalty: {distribution_penalty}, Duration Penalty: {total_time_unused}")
+        # scale the penalties
+        # total_time_unused = total_time_unused / total_time available*100
+        # time_penalty = time_penalty / total_time available*100
+        # author_penalty = author_penalty / total authors*100
+        # distribution_penalty = distribution_penalty / total papers*100
 
-        total_penalty = total_time_unused + time_penalty + author_penalty + distribution_penalty
+        # calcukate scaled penalties
+        total_time_available = sum(session.max_length for session in self.sessions)
+        weighted_time_penalty = time_penalty / total_time_available * 100
+        weighted_author_penalty = author_penalty / len(total_authors) * 100
+        weighted_distribution_penalty = distribution_penalty / num_papers * 100
+        weighted_total_time_unused = total_time_unused / total_time_available * 100
+    
+        # total_penalty
+        total_penalty = weighted_time_penalty + weighted_author_penalty + weighted_distribution_penalty + weighted_total_time_unused
 
-        return 1 / (1 + total_penalty)
+        fitness = 1 / (1 + total_penalty)
+
+        # return inididual penalties and fitness
+        return {
+            'weighted_time_penalty': weighted_time_penalty,
+            'weighted_author_penalty': weighted_author_penalty,
+            'weighted_distribution_penalty': weighted_distribution_penalty,
+            'weighted_total_time_unused': weighted_total_time_unused,
+            'total_penalty': total_penalty,
+            'fitness': fitness
+        }
 
 
     
@@ -76,8 +107,9 @@ class Solution:
                     
                         
 class Population:
-    def __init__(self, solutions):
+    def __init__(self, solutions, population_size):
         self.solutions = solutions  
+        self.population_size = population_size
 
     def print_population(self):
         for i, solution in enumerate(self.solutions):
@@ -87,7 +119,7 @@ class Population:
     def best_solution(self, num_papers):
         best_solution = self.solutions[0]
         for solution in self.solutions:
-            if solution.fitness(num_papers) > best_solution.fitness(num_papers):
+            if solution.fitness(num_papers)['fitness'] > best_solution.fitness(num_papers)['fitness']:
                 best_solution = solution
         return best_solution, best_solution.fitness(num_papers)
 
@@ -155,37 +187,65 @@ class GeneticAlgorithm:
         # else:  
         #     random.shuffle(session_to_mutate.tracks[track_index1])
 
-        swap_probability = 0.5
-        move_probability = 0.5
+        # swap_probability = 0.5
+        # move_probability = 0.5
 
-        session_to_mutate = random.choice(solution.sessions)
+        # print(solution.print_solution())
 
-        if random.random() < swap_probability:
-            if len(session_to_mutate.tracks) > 1:
+        solution_copy = copy.deepcopy(solution)
+        # check if session is empty i.e none of the tracks have any papers. select session_to_mutate as non empty session
+        session_to_mutate = random.choice(solution_copy.sessions)
+        while not any(session_to_mutate.tracks):
+            session_to_mutate = random.choice(solution_copy.sessions)
+
+
+        # if random.random() < swap_probability:
+        # print("Swap Mutation")
+        if len(session_to_mutate.tracks) > 1:
+            # swap two papers between two tracks
+            track1, track2 = 0, 0
+            while track1 == track2:
+                # print("Track1: ", track1, "Track2: ", track2)
                 track1, track2 = random.sample(session_to_mutate.tracks, 2)
-                if track1 and track2:
-                    paper1 = random.choice(track1)
-                    paper2 = random.choice(track2)
-                    track1.append(paper2)
-                    track2.append(paper1)
-                    track1.remove(paper1)
-                    track2.remove(paper2)   
+            
+            if track1 and track2:
+                paper1 = random.choice(track1)
+                paper2 = random.choice(track2)
+                track1.append(paper2)
+                track2.append(paper1)
+                track1.remove(paper1)
+                track2.remove(paper2)                
+                 
 
-        if random.random() < move_probability:
-            # move paper from one session to another
-            session_from = random.choice(solution.sessions)
-            session_to = random.choice(solution.sessions)
-            track_from = random.choice(session_from.tracks)
-            track_to = random.choice(session_to.tracks)
-            paper_to_move = random.choice(track_from)
-            track_to.append(paper_to_move)
-            track_from.remove(paper_to_move)
-        
+        # if random.random() < move_probability:
+        # move paper from one session to another
+        # print("Move Mutation")
+        session_from, session_to = 0, 0
+        while session_from == session_to:
+            session_from, session_to = random.sample(solution_copy.sessions, 2)
+            # print(session_from, session_to)
+
+        track_from = random.choice(session_from.tracks)
+        track_to = random.choice(session_to.tracks)
+        if track_from and track_to:
+            paper = random.choice(track_from)
+            track_to.append(paper)
+            track_from.remove(paper)
+
+        return solution_copy
+
+            # session_from = random.choice(solution.sessions)
+            # session_to = random.choice(solution.sessions)
+            # track_from = random.choice(session_from.tracks)
+            # track_to = random.choice(session_to.tracks)
+            # paper_to_move = random.choice(track_from)
+            # track_to.append(paper_to_move)
+            # track_from.remove(paper_to_move)
 
     def select_parents(self, population, num_papers, tournament_size=3):
         def tournament():
             contenders = random.sample(population.solutions, tournament_size)
-            best = sorted(contenders, key=lambda solution: solution.fitness(num_papers), reverse=True)[0]
+            best = sorted(contenders, key=lambda solution: solution.fitness(num_papers)['fitness'], reverse=True)[0]
             return best
 
         parent1 = tournament()
@@ -196,25 +256,44 @@ class GeneticAlgorithm:
 
     def select_survivors(self, population, new_population, num_papers):
         combined_population = population.solutions + new_population.solutions
-        sorted_combined_population = sorted(combined_population, key=lambda solution: solution.fitness(num_papers), reverse=True)
+        sorted_combined_population = sorted(combined_population, key=lambda solution: solution.fitness(num_papers)['fitness'], reverse=True)
         survivors = sorted_combined_population[:self.population_size]
-        return Population(survivors)
+        return Population(survivors, self.population_size)
+    
+    def plot_progress(self, fitness_values):
+        # plot graph of fitness values over generations
+        plt.plot(fitness_values)
+        plt.xlabel('Generation')    
+        plt.ylabel('Fitness Value')
+        plt.title('Fitness Value over Generations')
+        plt.show()
+        
 
     def run(self, papers, num_sessions, session_lengths, num_tracks, num_solutions):
         session_details = [(session_lengths[i], num_tracks[i]) for i in range(len(session_lengths))]
-
-        population = Population(self.create_population(papers, session_details, num_solutions))
-        for generation in range(self.max_generations):
+        fitness_values = []
+        population = Population(self.create_population(papers, session_details, num_solutions), num_solutions)
+        for generation in tqdm(range(self.max_generations)):
+            # print("Generation %d" % generation)
             new_population = []
-            for _ in range(self.population_size):
+            # implement elitism, keep 10% best population
+            population.solutions = sorted(population.solutions, key=lambda solution: solution.fitness(len(papers))['fitness'], reverse=True)
+            new_population.extend(population.solutions[:int(0.1 * self.population_size)])
+            while len(new_population) < self.population_size:
                 parent1, parent2 = self.select_parents(population, len(papers))
                 if random.random() < self.crossover_probability:
                     child = self.crossover(parent1, parent2)
                 else:
                     child = parent1
                 if random.random() < self.mutation_probability:
-                    self.mutate(child)
+                    child = self.mutate(child)
                 new_population.append(child)
-            population = self.select_survivors(population, Population(new_population), len(papers))
+            population = self.select_survivors(population, Population(new_population, self.population_size), len(papers))
+            # get best fitness
+            best_solution, fitness = population.best_solution(len(papers))
+            fitness_values.append(fitness['fitness'])
+            
+        # plot fitness values over generations
+        self.plot_progress(fitness_values)
         return population.best_solution(len(papers))
     
