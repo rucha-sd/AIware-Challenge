bfe0b02676411add8eaec8cfbc0c5b209d0fa063
Vigneshwaran <zeus@Vigneshwarans-Macbook-Pro.local>
2024-04-24 16:08:40 -0400

    final files

--- GA.py
+++ GA.py
@@ -11,12 +11,13 @@ class Paper:
         self.duration = duration
         topics = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         self.topic = random.choice(topics) if topic is None else topic
+    def print_paper(self):
+        return f"ID: {self.id}, Authors: {self.authors}, Duration: {self.duration}, Topic: {self.topic}"    
 
 class Session:
     def __init__(self, max_length, tracks):
         self.tracks = tracks  
         self.max_length = max_length  
-
 
 class Solution:
     def __init__(self, sessions):
@@ -133,8 +134,7 @@ class Solution:
                 for paper in track:
                     print(f"    Paper ID: {paper.id}, Authors: {', '.join(paper.authors)}, Topic: {paper.topic},  Duration: {paper.duration} minutes")
             print("-" * 40)
-                    
-                        
+                                            
 class Population:
     def __init__(self, solutions, population_size):
         self.solutions = solutions  
@@ -151,7 +151,6 @@ class Population:
             if solution.fitness(num_papers)['fitness'] > best_solution.fitness(num_papers)['fitness']:
                 best_solution = solution
         return best_solution, best_solution.fitness(num_papers)
-
 
 class GeneticAlgorithm:
     def __init__(self, population_size, max_generations, crossover_probability, mutation_probability):
@@ -299,7 +298,7 @@ class GeneticAlgorithm:
         survivors = sorted_combined_population[:self.population_size]
         return Population(survivors, self.population_size)
     
-    def plot_progress(self, fitness_values, time_penalties, author_penalties, distribution_penalties, coherence_penalties):
+    def plot_progress(self, fitness_values, time_penalties, author_penalties, distribution_penalties, coherence_penalties, year):
         fig, axs = plt.subplots(2, 2)
         # increase figure size
         fig.set_size_inches(12, 8)
@@ -312,7 +311,7 @@ class GeneticAlgorithm:
         axs[1, 0].set_title('Weighted Distribution Penalty')
         axs[1, 1].plot(coherence_penalties)
         axs[1, 1].set_title('Weighted Coherence Penalty')
-        plt.savefig(f'fitness_values_{self.population_size}_{self.max_generations}_2.png')
+        plt.savefig(f'fitness_values_{self.population_size}_{self.max_generations}_{year}_2.png')
         plt.show()
         # new plot
         plt.clf()
@@ -322,11 +321,11 @@ class GeneticAlgorithm:
         plt.ylabel('Fitness Value')
         plt.title('Fitness Value over Generations')
         # save with pop_size, num_generations
-        plt.savefig(f'fitness_values_{self.population_size}_{self.max_generations}_11.png')
+        plt.savefig(f'fitness_values_{self.population_size}_{self.max_generations}_{year}_1.png')
         plt.show()
         
 
-    def run(self, papers, num_sessions, session_lengths, num_tracks, num_solutions):
+    def run(self, papers, num_sessions, session_lengths, num_tracks, num_solutions, year):
         session_details = [(session_lengths[i], num_tracks[i]) for i in range(len(session_lengths))]
         fitness_values = []
         time_penalties = []
@@ -359,6 +358,6 @@ class GeneticAlgorithm:
             coherence_penalties.append(fitness['weighted_coherence_penalty'])
             
         # plot fitness values over generations
-        self.plot_progress(fitness_values, time_penalties, author_penalties, distribution_penalties, coherence_penalties)
+        self.plot_progress(fitness_values, time_penalties, author_penalties, distribution_penalties, coherence_penalties, year)
         return population.best_solution(len(papers))
     
--- pipeline.py
+++ pipeline.py
@@ -15,17 +15,29 @@ if __name__ == '__main__':
 
     print(f'population_size: {population_size} max_generations: {max_generations}')
 
-    parsed_input_file = preprocess_data.parse_csv_to_array(input_file)
+    parsed_input_file, papers_schedule = preprocess_data.parse_csv_to_array(input_file)
     session_durations, num_tracks_per_session = preprocess_data.session_details(parsed_input_file)
-    print(session_durations[0])
     session_details = [(session_durations[i], num_tracks_per_session[i]) for i in range(len(session_durations))]
     # print(num_tracks_per_session)
     ga = GA.GeneticAlgorithm(population_size, max_generations, crossover_probability, mutation_probability)
-    input = pd.read_csv("input.csv")
-    papers = preprocess_data.create_papers(input_file)
+    papers_dict = preprocess_data.create_papers(input_file)
+    papers = papers_array = [x for x in papers_dict.values()]
     initial_population = ga.create_population(papers, session_details, population_size)
     pop = GA.Population(initial_population, population_size)
-    pop = ga.run(papers, len(num_tracks_per_session), session_durations, num_tracks_per_session, population_size)
+    pop = ga.run(papers, len(num_tracks_per_session), session_durations, num_tracks_per_session, population_size, args[4])
     pop[0].print_solution()
     print(pop[1])
     
+    solutions_from_csv = preprocess_data.merge_overlaps(papers_schedule)
+    solutions = preprocess_data.create_solution(solutions_from_csv, papers_dict)
+
+    #printing our solutions 
+    print("ideal solution")        
+    for time_slot, rooms in solutions.items():
+        print("###############")
+        print(f"Time Slot: {time_slot}")
+        for room, papers in rooms.items():
+            print(room)
+            for paper in papers:
+                print(paper.print_paper())
+            print("\n")
